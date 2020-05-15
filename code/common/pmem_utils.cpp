5b88a63cf6e62faaf8b30c49492c290053ea4a20
KokorinIlya <kokorin.ilya.1998@yandex.ru>
2020-05-15 15:54:36 +0300

    more flush tests

--- code/common/pmem_utils.cpp
+++ code/common/pmem_utils.cpp
@@ -28,14 +28,13 @@ uint64_t get_cache_line_aligned_address(uint64_t address)
      */
     const uint64_t mask = ~(CACHE_LINE_SIZE - 1);
     assert(mask % CACHE_LINE_SIZE == 0);
-    uint64_t result = (address & mask) + CACHE_LINE_SIZE;
+    const uint64_t result = (address & mask) + CACHE_LINE_SIZE;
     assert(result % CACHE_LINE_SIZE == 0);
     return result;
 }
 
 bool is_stack_address(const uint8_t* address)
 {
-    const uint8_t* stack_begin_address =
-            thread_local_non_owning_storage<persistent_stack>::ptr->get_stack_ptr();
+    const uint8_t* const stack_begin_address = thread_local_non_owning_storage<persistent_stack>::ptr->get_stack_ptr();
     return address >= stack_begin_address && address < stack_begin_address + PMEM_STACK_SIZE;
 }
