37a7bf60eb05dd8e4c86a83dcf2312a96007beab
KokorinIlya <kokorin.ilya.1998@yandex.ru>
2020-05-17 22:47:57 +0300

    doc added

--- code/storage/global_non_owning_storage.h
+++ code/storage/global_non_owning_storage.h
@@ -1,7 +1,17 @@
 #ifndef DIPLOM_GLOBAL_NON_OWNING_STORAGE_H
 #define DIPLOM_GLOBAL_NON_OWNING_STORAGE_H
 
-// TODO: write doc
+/**
+ * Stores pointers to instances of some object of type T as global singleton object.
+ * For all program threads, only one instance of T is stored, but instances of this class
+ * for different types (for example, global_non_owning_storage<int> and global_non_owning_storage<string>)
+ * store pointers to different objects.
+ * Storage doesn't own objects, pointer to which is stored in storage.
+ * Since there is only one global object in the system and all system threads can access it,
+ * user should himself ensure correctness of access to the stored object
+ * (just like when using global_storage<T>).
+ * @tparam T - class of object, pointer to which will be stored.
+ */
 template <typename T>
 struct global_non_owning_storage
 {
