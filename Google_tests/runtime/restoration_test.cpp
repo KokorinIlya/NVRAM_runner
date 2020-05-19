347d37db5f5f490549a07a4021b46191fdf01a34
KokorinIlya <kokorin.ilya.1998@yandex.ru>
2020-05-19 15:28:10 +0300

    restoration tests started

---
+++ Google_tests/runtime/restoration_test.cpp
@@ -0,0 +1,71 @@
+#include "gtest/gtest.h"
+#include "../../code/persistent_stack/call.h"
+#include "../../code/model/system_mode.h"
+#include "../../code/model/function_address_holder.h"
+#include "../../code/storage/global_storage.h"
+#include "../common/test_utils.h"
+#include "../../code/runtime/restoration.h"
+
+namespace
+{
+    bool f_executed = false;
+    bool g_executed = false;
+    bool h_executed = false;
+
+    void f(uint8_t const*)
+    {
+        do_call("g", std::vector<uint8_t>());
+    }
+
+    void g(uint8_t const*)
+    {
+        do_call("h", std::vector<uint8_t>());
+    }
+
+    void h(uint8_t const*)
+    {
+        throw std::runtime_error("ha-ha, system crash go brrrrr");
+    }
+
+    void f_recover(uint8_t const*)
+    {
+        f_executed = true;
+    }
+
+    void g_recover(uint8_t const*)
+    {
+        g_executed = true;
+    }
+
+    void h_recover(uint8_t const*)
+    {
+        h_executed = true;
+    }
+}
+
+TEST(restoration, simple)
+{
+    temp_file stack_file(get_temp_file_name("stack"));
+    persistent_memory_holder stack(stack_file.file_name, false, PMEM_STACK_SIZE);
+
+    global_storage<function_address_holder>::set_object(function_address_holder());
+    global_storage<function_address_holder>::get_object().funcs["f"] = {f, f_recover};
+    global_storage<function_address_holder>::get_object().funcs["g"] = {g, g_recover};
+    global_storage<function_address_holder>::get_object().funcs["h"] = {h, h_recover};
+
+    thread_local_owning_storage<ram_stack>::set_object(ram_stack());
+    thread_local_non_owning_storage<persistent_memory_holder>::ptr = &stack;
+    global_storage<system_mode>::set_object(system_mode::RECOVERY);
+
+    try
+    {
+        do_call("f", std::vector<uint8_t>());
+    }
+    catch (...)
+    {}
+
+    do_restoration(stack);
+    EXPECT_FALSE(f_executed);
+    EXPECT_TRUE(g_executed);
+    EXPECT_TRUE(h_executed);
+}
